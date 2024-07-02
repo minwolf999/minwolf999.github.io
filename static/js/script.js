5f3f16417aaefc2abe98160812433720c0022a47
anmarvin <minwolf.999@gmail.com>
2024-07-02 15:25:37 +0200

    split in different function

--- static/js/script.js
+++ static/js/script.js
@@ -6,9 +6,15 @@ async function GetJSON(url) {
     return await fetch(url).then(data => data.json()).catch(err => {console.log(err);})
 }
 
-function createElement(text, responses) {
+async function StartGame() {
+    script = await GetJSON('./static/json/script.json')
+
+    DisplayElement(...script[0])
+}
+
+function DisplayElement(text, responses) {
     document.body.innerHTML = ''
-    const div = document.createElement('div')
+    var div = document.createElement('div')
     
     if (responses[0].length === 2) {
         div.textContent = text
@@ -31,45 +37,46 @@ function createElement(text, responses) {
                     comm_responses++
                 }
     
-                createElement(...script[responses[i][1]])
+                DisplayElement(...script[responses[i][1]])
             })
         }
 
         document.body.appendChild(Responses)
     } else {
-        div.className = "RH_Question"
-
-        if (comm_responses > rh_responses) {
-            div.innerHTML = `Au vus de vos réponses vous semblez être orienté vers un profil commercial. Vous
-                excellez dans les stratégies de placement, la négociation avec les entreprises
-                clientes et l'analyse de marché.
-                <br><br>
-                Score RH: ${rh_responses}
-                <br>
-                Score Commercial: ${comm_responses}`
-        } else if (rh_responses > comm_responses) {
-            div.innerHTML = `Au vus de vos réponses vous avez une inclinaison pour un profil RH. Vous êtes
-                doué pour la gestion des talents intérimaires, la médiation des conflits et le
-                développement des compétences
-                <br><br>
-                Score RH: ${rh_responses}
-                <br>
-                Score Commercial: ${comm_responses}`
-        } else {
-            div.innerHTML = `Au vus de vos réponses vous avez un profil mixe. Vous êtes aussi doué en stratégie 
-                de placement ainsi qu'en gestion de talent. 
-                <br><br>
-                Score RH: ${rh_responses}
-                <br>
-                Score Commercial: ${comm_responses}`
-        }
+        div = endGame(div)
     }
 
     document.body.appendChild(div)
 }
 
-async function StartGame() {
-    script = await GetJSON('./static/json/script.json')
+function endGame(div) {
+    div.className = "RH_Question"
 
-    createElement(...script[0])
+    if (comm_responses > rh_responses) {
+        div.innerHTML = `Au vus de vos réponses vous semblez être orienté vers un profil commercial. Vous
+            excellez dans les stratégies de placement, la négociation avec les entreprises
+            clientes et l'analyse de marché.
+            <br><br>
+            Score RH: ${rh_responses}
+            <br>
+            Score Commercial: ${comm_responses}`
+    } else if (rh_responses > comm_responses) {
+        div.innerHTML = `Au vus de vos réponses vous avez une inclinaison pour un profil RH. Vous êtes
+            doué pour la gestion des talents intérimaires, la médiation des conflits et le
+            développement des compétences
+            <br><br>
+            Score RH: ${rh_responses}
+            <br>
+            Score Commercial: ${comm_responses}`
+    } else {
+        div.innerHTML = `Au vus de vos réponses vous avez un profil mixe. Vous êtes aussi doué en stratégie 
+            de placement ainsi qu'en gestion de talent. 
+            <br><br>
+            Score RH: ${rh_responses}
+            <br>
+            Score Commercial: ${comm_responses}`
+    }
+
+    return div
 }
+
